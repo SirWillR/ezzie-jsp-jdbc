af063d5c7bf5aee266b055d4afb6f65f79d1fa09
DSirWill <dev_will@hotmail.com>
2019-04-03 14:59:06 -0300

    Adicionado funções de LocalizaçãoDAO

--- src/main/java/model/dao/LocalizacaoDAO.java
+++ src/main/java/model/dao/LocalizacaoDAO.java
@@ -12,5 +12,8 @@ public interface LocalizacaoDAO {
 	List<Pais> findAllPais();
 	List<Estado> findAllEstado(int idPais);
 	List<Cidade> findAllCity(int idEstado);
+	Pais findPaisById(int idPais);
+	Estado findEstadoById(int idEstado);
+	Cidade findCityById(int idCity);
 	Localizacao findById(int idCity);
 }
